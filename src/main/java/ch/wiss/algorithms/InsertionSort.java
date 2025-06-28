8affb49b51ec8e402d95890d22c6220427be2247
itmeJanHelbling <zolo_2010@hotmail.ch>
2025-06-28 10:50:12 +0200

    InsertionSort vervollständigt

--- src/main/java/ch/wiss/algorithms/InsertionSort.java
+++ src/main/java/ch/wiss/algorithms/InsertionSort.java
@@ -1,5 +1,35 @@
 package ch.wiss.algorithms;
 
-public class InsertionSort {
-    
+import java.util.List;
+
+import ch.wiss.magicsort.ISort;
+
+public class InsertionSort implements ISort {
+
+    @Override
+    public List<Integer> sort(List<Integer> list) {
+        if (list == null || list.size() < 2) {
+            return list;
+        }
+
+        // In-place Insertion Sort
+        for (int i = 1; i < list.size(); i++) { //
+            int key = list.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && list.get(j) > key) {
+                list.set(j + 1, list.get(j)); // Schiebe Element nach rechts
+                j--;
+            }
+
+            list.set(j + 1, key); // Füge key an der richtigen Stelle ein
+        }
+
+        return list; // In-place sortiert
+    }
+
+    @Override
+    public String getAlgorithmName() {
+        return "insertion";
+    }
 }
