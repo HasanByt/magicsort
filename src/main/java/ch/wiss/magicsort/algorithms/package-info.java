b0b5b8717f619f32b2259a051e1e80d32971d295
itmeJanHelbling <zolo_2010@hotmail.ch>
2025-06-28 15:08:40 +0200

    package-info.java für alle packages hinzugefügt.

---
+++ src/main/java/ch/wiss/magicsort/algorithms/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Dieses Package enthält die Implementierungen verschiedener
+ * Sortieralgorithmen wie z.B. BubbleSort, InsertionSort und BinaryTreeSort.
+ */
+package ch.wiss.magicsort.algorithms;
---
+++ src/main/java/ch/wiss/magicsort/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Dieses Package enthält die Hauptanwendung und die zentrale Steuerung
+ * der Sortieralgorithmen im Projekt magicsort.
+ */
+package ch.wiss.magicsort;
