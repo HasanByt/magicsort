38312aac3f4827be91cd595c7e8fdf6b97124f96
8ytha <hasanbytyqi@gmx.ch>
2025-07-12 00:46:37 +0200

    adapted for magicsort

--- src/main/java/ch/wiss/magicsort/SortAlgorithm.java
+++ src/main/java/ch/wiss/magicsort/SortAlgorithm.java
@@ -1,12 +1,31 @@
 package ch.wiss.magicsort;
 
+import java.util.Arrays;
+
 public enum SortAlgorithm {
-    BINARY_TREE,
-    BUBBLE,
-    GNOME,
-    INSERTION,
-    MERGE,
-    SHAKER,
-    SLOW,
-    TIM
+    BINARY_TREE("BinaryTreeSort"),
+    BUBBLE("BubbleSort"),
+    GNOME("GnomeSort"),
+    INSERTION("InsertionSort"),
+    MERGE("MergeSort"),
+    SHAKER("ShakerSort"),
+    SLOW("SlowSort"),
+    TIM("TimSort");
+
+    private final String dbName;
+
+    SortAlgorithm(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public static SortAlgorithm fromDbName(String name) {
+        return Arrays.stream(values())
+                .filter(a -> a.dbName.equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unbekannter Algorithmus: " + name));
+    }
 }
