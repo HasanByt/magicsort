c420bc5e4c0cb953bea10b28dcdb3cde164655bc
Hasan <hasanbytyqi@gmx.ch>
2025-07-21 08:28:41 +0000

    Bugfix

--- src/main/java/ch/wiss/magicsort/SortResult.java
+++ src/main/java/ch/wiss/magicsort/SortResult.java
@@ -2,26 +2,4 @@ package ch.wiss.magicsort;
 
 import java.util.List;
 
-public class SortResult {
-    private final List<Integer> sorted;
-    private final long comparisons;
-    private final double durationMs;
-
-    public record SortResult(List<Integer> sorted, long comparisons, double durationMs) {
-        this.sorted = sorted;
-        this.comparisons = comparisons;
-        this.durationMs = durationMs;
-    }
-
-    public List<Integer> getSorted() {
-        return sorted;
-    }
-
-    public long getComparisons() {
-        return comparisons;
-    }
-
-    public double getDurationMs() {
-        return durationMs;
-    }
-}
+public record SortResult(List<Integer> sorted, long comparisons, double durationMs) {}
